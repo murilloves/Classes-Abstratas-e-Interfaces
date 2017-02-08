8a922c49f6c5f33eac8d4430304a81bbb36bea78
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2017-02-08 13:26:05 -0200

    Create Registro.java

---
+++ Registro.java
@@ -0,0 +1,7 @@
+package ClassesAbstrataEInterfaces;
+
+// 1 – Interface Registro com os métodos registra_abertura_dia e registra_fechamento_dia.
+public interface Registro {
+    public void registra_abertura_dia();
+    public void registra_fechamento_dia();
+}
