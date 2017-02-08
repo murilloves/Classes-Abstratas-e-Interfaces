1ec40aa3bed026f345c8e6368fb91329316ed5e0
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2017-02-08 13:24:10 -0200

    Create AbstrEInterf.java

---
+++ AbstrEInterf.java
@@ -0,0 +1,12 @@
+package ClassesAbstrataEInterfaces;
+/**
+ *
+ * @author Murillo Vieira
+ */
+public class AbstrEInterf {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
